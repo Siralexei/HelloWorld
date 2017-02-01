ed8d1983f2728403b1ae4ecb6ee1c0b0f4cf3ebc
Alexi <Alexi@192.168.1.151>
2017-02-01 00:49:45 -0300

    Se agrega proyecto test.

---
+++ test/src/main/java/org/alexi/javabrains/test/App.java
@@ -0,0 +1,13 @@
+package org.alexi.javabrains.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
